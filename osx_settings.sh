909a057b414aea605495509819fa6003c6a6a70b
Paul R Alexander <palexander@stanford.edu>
2014-10-22 13:00:37 -0700

    Update osx_settings.sh

--- osx_settings.sh
+++ osx_settings.sh
@@ -1,32 +1,80 @@
-#Enable repeat on keydown
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+
+# Expand save panel by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+
+# Trackpad: enable tap to click for this user and for the login screen
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+# Enable full keyboard access for all controls
+# (e.g. enable Tab in modal dialogs)
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+# Disable auto-correct
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+ 
+# Require password immediately after sleep or screen saver begins
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+# Disable the warning when changing a file extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+
+# Set dock magnification size
+defaults write com.apple.dock largesize -float 96.0
+killall Dock
+
+# Save to disk (not to iCloud) by default
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+# Remove duplicates in the “Open With” menu (also see `lscleanup` alias)
+/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user
+
+# Disable smart quotes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+
+# Disable smart dashes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+# Enable repeat on keydown
 defaults write -g ApplePressAndHoldEnabled -bool false
 
-#Use current directory as default search scope in Finder
+# Use current directory as default search scope in Finder
 defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 
-#Show Path bar in Finder
+# Show Path bar in Finder
 defaults write com.apple.finder ShowPathbar -bool true
 
-#Show Status bar in Finder
+# Show Status bar in Finder
 defaults write com.apple.finder ShowStatusBar -bool true
 
-#Show indicator lights for open applications in the Dock
+# Show indicator lights for open applications in the Dock
 defaults write com.apple.dock show-process-indicators -bool true
 
-#Enable AirDrop over Ethernet and on unsupported Macs running Lion
+# Enable AirDrop over Ethernet and on unsupported Macs running Lion
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
 
-#Set a blazingly fast keyboard repeat rate
+# Set a blazingly fast keyboard repeat rate
 defaults write NSGlobalDomain KeyRepeat -int 0.02
 
-#Set a shorter Delay until key repeat
+# Set a shorter Delay until key repeat
 defaults write NSGlobalDomain InitialKeyRepeat -int 12
 
-#Enable Safari’s debug menu
+# Enable Safari’s debug menu
 defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
 
-#Add a context menu item for showing the Web Inspector in web views
+# Add a context menu item for showing the Web Inspector in web views
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 
-#Show the ~/Library folder
+# Show the ~/Library folder
 chflags nohidden ~/Library
