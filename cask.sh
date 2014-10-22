5b97db1ebe42a192eb14fa16ef17f2680eb824e8
Paul R Alexander <palexander@stanford.edu>
2014-10-22 12:43:37 -0700

    Brew cask install list

---
+++ cask.sh
@@ -0,0 +1,25 @@
+brew cask install \
+firefox \
+firefox-aurora \
+google-chrome-beta \
+iterm2-beta \
+sequel-pro \
+textmate \
+skype \
+spotify \
+alfred \
+dropbox \
+vagrant \
+virtualbox \
+sublime-text3 \
+virtualbox \
+bartender \
+aerofs \
+dashlane \
+mjolnir \
+rubymine \
+steam \
+github \
+intellij-idea-ce \
+vmware-fusion-6 \
+stay \
