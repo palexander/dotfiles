3ef63bc1553fbe4db24aca89eb54adc68a79bb8f
Paul R Alexander <palexander@stanford.edu>
2014-10-24 11:31:17 -0700

    Script to create symlinks in home dir

---
+++ symlinks.sh
@@ -0,0 +1,6 @@
+ln -s $HOME/src/dotfiles/zshrc $HOME/.zshrc
+ln -s $HOME/src/dotfiles/profile $HOME/.profile
+mkdir -p $HOME/Library/Application\ Support/Sublime\ Text\ 3
+ln -s $HOME/Dropbox/Profile/ST3/Backup $HOME/Library/Application\ Support/Sublime\ Text\ 3/
+ln -s $HOME/Dropbox/Profile/ST3/Installed\ Packages $HOME/Library/Application\ Support/Sublime\ Text\ 3/
+ln -s $HOME/Dropbox/Profile/ST3/Packages $HOME/Library/Application\ Support/Sublime\ Text\ 3/
