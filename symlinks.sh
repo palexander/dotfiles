46950195dd5048291ba5c672512f80e29639d00c
Paul R Alexander <palexander@stanford.edu>
2014-10-25 23:13:29 -0700

    symlink for tmux

--- symlinks.sh
+++ symlinks.sh
@@ -4,3 +4,4 @@ mkdir -p $HOME/Library/Application\ Support/Sublime\ Text\ 3
 ln -s $HOME/Dropbox/Profile/ST3/Backup $HOME/Library/Application\ Support/Sublime\ Text\ 3/
 ln -s $HOME/Dropbox/Profile/ST3/Installed\ Packages $HOME/Library/Application\ Support/Sublime\ Text\ 3/
 ln -s $HOME/Dropbox/Profile/ST3/Packages $HOME/Library/Application\ Support/Sublime\ Text\ 3/
+ln -s $HOME/src/dotfiles/tmux $HOME/.tmux.conf
