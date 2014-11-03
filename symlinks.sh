1b8302ce77f0d2165a437278f8bab104ec896ac1
Paul R Alexander <palexander@stanford.edu>
2014-11-03 11:46:28 -0800

    New symlinks for tmux and git config

--- symlinks.sh
+++ symlinks.sh
@@ -5,3 +5,5 @@ ln -s $HOME/Dropbox/Profile/ST3/Backup $HOME/Library/Application\ Support/Sublim
 ln -s $HOME/Dropbox/Profile/ST3/Installed\ Packages $HOME/Library/Application\ Support/Sublime\ Text\ 3/
 ln -s $HOME/Dropbox/Profile/ST3/Packages $HOME/Library/Application\ Support/Sublime\ Text\ 3/
 ln -s $HOME/src/dotfiles/tmux $HOME/.tmux.conf
+hardlink $HOME/src/dotfiles/zsh_history $HOME/.zsh_history
+ln -s $HOME/src/dotfiles/gitconfig $HOME/.gitconfig
